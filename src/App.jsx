bcd775a08350b5f3c53bad8a592e8fe3ec99e600
Ahmed Mansour <ahmed@mechanizm.games>
2023-07-04 17:45:17 -0400

    New Tasks and Task list
    - React state to manage new tasks tasks
    - React state to manage task list
    - Added hooks to update the display
    - TODO: enable delete button

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,47 @@
+import { useState } from "react"
+import "./styles.css"
+
+
 export default function App() {
-  return "HI"
+  const [newTask, setNewTask] = useState("")
+  const [tasks, setTasks] = useState([])
+  
+  function insertTask(e) {
+    e.preventDefault()
+    setTasks(oldTasks => {
+      return [
+        ...oldTasks, {id: crypto.randomUUID(),
+        title: newTask,
+        completed: false},
+      ]
+    })
+  }
+
+  return <>
+  <form onSubmit={insertTask} className="new-item-form">
+    <div className="form-row">
+      <label htmlFor="task">New task</label>
+      <input
+      value={newTask}
+      onChange={e => setNewTask(e.target.value)}
+      type="text" id="task"/>
+    </div>
+    <button className="btn">Add</button>
+  </form>
+  <h1 className="header">Task List</h1>
+  <ul className="list">
+    {tasks.map(task => {
+      return <li>
+        <label>
+          <input type="checkbox" checked={task.completed}/>
+          {task.title}
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+    }) }
+    
+    <li>Tomater</li>
+  </ul>
+  HI
+  </>
 }
